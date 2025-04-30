9970f687e0f04b1d4d2ce818723f1196300143cc
Anton Kostiuk <antkos90@gmail.com>
2025-04-30 19:22:17 +0200

    fixed

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,3 +1,7 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-export const PATH_DB = path.join('src', 'db', 'db.json');
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export const PATH_DB = path.join(dirname, '../db/db.json');
